b4e6ea3af806fd9c63b098ce83b0a240a6dd26a2
Pablo DS <pablodariozsh@gmail.com>
2026-05-23 11:46:41 -0600

    Update for deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,7 +22,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://rec-system-v2.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
