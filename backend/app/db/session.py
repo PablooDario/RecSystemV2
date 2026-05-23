8a1563f4c4a329d30f319d921894d7bf3bcf7fb8
Pablo DS <pablodariozsh@gmail.com>
2026-05-23 15:09:27 -0600

    Fix numpy types for psycopg2

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -2,6 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.core.config import DATABASE_URL
 
+import numpy as np
+from psycopg2.extensions import register_adapter, AsIs
+
+# Fix numpy types for psycopg2
+register_adapter(np.int64, lambda val: AsIs(int(val)))
+register_adapter(np.int32, lambda val: AsIs(int(val)))
+register_adapter(np.float64, lambda val: AsIs(float(val)))
+register_adapter(np.float32, lambda val: AsIs(float(val)))
+
 # Connection to the database
 engine = create_engine(DATABASE_URL)
 
